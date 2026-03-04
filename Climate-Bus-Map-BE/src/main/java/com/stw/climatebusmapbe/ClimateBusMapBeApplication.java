b564b434c0931097dced40b8beb5f1516a681f6a
STW5 <xodnd8384@gmail.com>
2026-03-04 16:05:59 +0900

    init commit

---
+++ Climate-Bus-Map-BE/src/main/java/com/stw/climatebusmapbe/ClimateBusMapBeApplication.java
@@ -0,0 +1,13 @@
+package com.stw.climatebusmapbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateBusMapBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimateBusMapBeApplication.class, args);
+	}
+
+}
---
+++ Climate-Bus-Map-BE/src/test/java/com/stw/climatebusmapbe/ClimateBusMapBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stw.climatebusmapbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateBusMapBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
